e710d3657d9ab193b99102605b12a877466eefc4
Steven Bondoux <bondoux.s@gmail.com>
2024-08-12 15:20:39 +0200

    Add style for error page

--- src/components/error.jsx
+++ src/components/error.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import { useRouteError } from 'react-router-dom'
 import { Link } from "react-router-dom"
+import '../assets/style/error.scss'
 
 export default function ErrorUrl() {
     const error = useRouteError()
     
   return (
-    <>
-    <div>
-         '404'
+    <div className='body-error'>
+    <div className='error'>
+         404
     </div>
-    <div>'Oups! La page que vous demandez n'existe pas.'</div>
+    <div className='message'>Oups! La page que vous demandez n'existe pas.</div>
     <Link to="/">
-        <p>Retour à la page d'acceuil</p>
+        <p className='message-return'>Retour à la page d'acceuil</p>
       </Link>
-    </>
+      </div>
   )
 }
